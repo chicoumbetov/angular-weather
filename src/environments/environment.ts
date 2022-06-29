2e4eecbb07e5b9d5cfe4c64c66a66f63701a118e
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-06-29 11:58:33 +0200

    :construction: environment API data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl:
+    'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '1a9b1bdc69mshcce5144abeb5d98p197886jsn27ae78319637',
 };
 
 /*
